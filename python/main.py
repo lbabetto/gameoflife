afe6174db3080e3e4d7819a016abfcdfa48e7325
Luca Babetto <l.babetto@cineca.it>
2023-11-27 17:11:40 +0100

    implemented MPI version (still WIP)

--- python/main.py
+++ python/main.py
@@ -1,10 +1,11 @@
 import pygame
 import numpy as np
-from itertools import product
-
 from typing import Tuple
 
-WIDTH, HEIGHT = 300, 200
+################
+# Pygame setup #
+################
+WIDTH, HEIGHT = 200, 100
 PIXEL_SIZE = 5
 
 # defining colours
@@ -12,126 +13,127 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (100, 100, 100)
 
-# Initializing grid with random 1s and 0s
-fraction_alive = 0.1
-GRID = np.random.choice([1, 0], size=(WIDTH, HEIGHT), p=[fraction_alive, 1 - fraction_alive])
+#############
+# MPI setup #
+#############
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+nprocs = comm.Get_size()
+rank = comm.Get_rank()
+
+# dividing cells among processes
+N = WIDTH * HEIGHT
+rem = N % nprocs
+nitems = (N - rem) // nprocs
+if rank < rem:
+    nitems += 1
+
+# calculating starting index
+start = rank * nitems
+if rank >= rem:
+    start += rem
+
+################
+# Pygame setup #
+################
+if rank == 0:
+    pygame.init()
+    pygame.font.init()
+
+    FONT = pygame.font.SysFont("consolas.ttf", 24)
+    SCREEN = pygame.display.set_mode((WIDTH * PIXEL_SIZE, HEIGHT * PIXEL_SIZE))
+    CLOCK = pygame.time.Clock()
+    pygame.display.set_caption("Game of Life")
+
+    SCREEN.fill(BLACK)
+    pygame.display.flip()
 
 
-def draw_pixel(coords: Tuple[int, int], color: Tuple[int, int, int], hollow: bool = False):
-    """Draw a pixel at position (x,y) of a given color, optionally hollow/filled
+def draw_pixel(position: int, color: Tuple[int, int, int], hollow: bool = False) -> None:
+    """Draw a cell at position `position % WIDTH, position // WIDTH` of a given color, optionally hollow/filled
 
     Parameters
     ----------
-    coords : Tuple[int, int]
-        (x,y) coords on grid
+    position : int
+        position of the cell on the grid
     color : Tuple[int, int, int]
         RGB code of the wanted color
     hollow : bool, optional
         toggles hollow (False) or filled (True) pixel, by default False
     """
-    x, y = coords
-
+    x, y = position % WIDTH, position // WIDTH
     rect = pygame.Rect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE)
     pygame.draw.rect(SCREEN, color, rect, 1 if hollow else 0)
 
 
-def draw_grid() -> None:
-    """Draw a grey grid"""
-    for x, y in product(range(WIDTH), range(HEIGHT)):
-        draw_pixel((x, y), GREY, True)
-
-
-def count_neighbours(coords: Tuple[int, int]) -> int:
-    """Count the number of neighbouring alive cells
-
-    Parameters
-    ----------
-    coords : Tuple[int, int]
-        (x,y) coords on grid
-
-    Return
-    ------
-    int
-        number of neighbouring live cells
-    """
-    x, y = coords
-    neighbours = 0
-
-    for dx, dy in product([-1, 0, 1], [-1, 0, 1]):
-        if dx == 0 and dy == 0:
-            continue
-        neighbours += GRID[(x + dx) % WIDTH][(y + dy) % HEIGHT]
-
-    return int(neighbours)
-
-
-def next_step(coords: Tuple[int, int]):
-    """Calculate alive/dead cells for the next step
-
-    Parameters
-    ----------
-    coords : Tuple[int, int]
-        (x,y) coords on grid
-
-
-    Return
-    NDArray[float64]
-        grid at the next step
-    ------
-    """
-    x, y = coords
-
-    neighbours = count_neighbours(coords)
-
-    if GRID[x][y] == 1:
-        if neighbours in [2, 3]:
-            NEWGRID[x][y] = 1
-    else:
-        if neighbours == 3:
-            NEWGRID[x][y] = 1
-
-
-pygame.init()
-pygame.font.init()
-FONT = pygame.font.SysFont("consolas.ttf", 24)
-SCREEN = pygame.display.set_mode((WIDTH * PIXEL_SIZE, HEIGHT * PIXEL_SIZE))
-CLOCK = pygame.time.Clock()
-pygame.display.set_caption("Game of Life")
-SCREEN.fill(BLACK)
-
-pygame.display.flip()
-
-
 def main():
-    global GRID
-    time = 0
+    # Initializing grid with random 1s and 0s
+    if rank == 0:
+        time = 0
+        fraction_alive = 0.2
+        GRID = np.array(
+            np.random.choice([1, 0], size=(WIDTH * HEIGHT), p=[fraction_alive, 1 - fraction_alive]),
+            dtype=np.ushort,
+        )
+    else:
+        GRID = np.zeros((WIDTH * HEIGHT), dtype=np.ushort)
+    comm.Bcast(GRID, root=0)
 
     while True:
-        # drawing cells
-        for x, y in product(range(WIDTH), range(HEIGHT)):
-            rect = pygame.Rect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE)
-            pygame.draw.rect(SCREEN, WHITE if GRID[x][y] == 1 else BLACK, rect)
+        if rank == 0:
+            for i in range(WIDTH * HEIGHT):
+                draw_pixel(start + i, WHITE if GRID[i] == 1 else BLACK, hollow=False)
 
-        # setting up next step
-        global NEWGRID
-        NEWGRID = np.zeros((WIDTH, HEIGHT))
-        for x, y in product(range(WIDTH), range(HEIGHT)):
-            next_step((x, y))
-        GRID = NEWGRID
+        NEWGRID = np.zeros(nitems, dtype=np.ushort)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+        # calculate neighbours and store data in vector
+        for i in range(nitems):
+            neighbours = 0
+
+            for step in [
+                -1,
+                # 0, avoid self-interaction
+                +1,
+                WIDTH - 1,
+                WIDTH,
+                WIDTH + 1,
+                -WIDTH - 1,
+                -WIDTH,
+                -WIDTH + 1,
+            ]:
+                neighbours += GRID[
+                    (start + i + step + WIDTH * HEIGHT) % (WIDTH * HEIGHT)
+                ]  # WIDTH * HEIGHT needed for periodicity
+
+            NEWGRID[i] = 0
+            if GRID[i] == 1:
+                if neighbours in [2, 3]:
+                    NEWGRID[i] = 1
+            else:
+                if neighbours == 3:
+                    NEWGRID[i] = 1
+
+        comm.Allgatherv(NEWGRID, GRID)
 
         # drawing background grid
-        draw_grid()
+        if rank == 0:
+            for i in range(WIDTH * HEIGHT):
+                draw_pixel(start + i, GREY, hollow=True)
 
-        TEXT = FONT.render(f"Time: {time}", True, (0, 255, 0))
-        SCREEN.blit(TEXT, (0, 0))
-        pygame.display.update()
-        CLOCK.tick(100)
-        time += 1
+        # checking for exit condition and displaying clock
+        if rank == 0:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            TEXT = FONT.render(f"Time: {time}", True, (0, 255, 0))
+            SCREEN.blit(TEXT, (0, 0))
+            pygame.display.update()
+            CLOCK.tick(100)
+
+            time += 1
 
 
 if __name__ == "__main__":
